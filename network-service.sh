c9f6d7920ee41569dbf9e8bb0efc29e07f309b43
Priyadeep Sen <work.priyadeepsen@gmail.com>
2025-09-17 08:28:55 +0530

    First Commit

---
+++ network-service.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#===============================================================================
+# SCRIPT: 	network-service.sh
+# AUTHOR: 	github.com/priyadeepsenwork
+# DESCRIPTION: 	Network and Service Health Monitor,
+#		Pings external hosts and checks local service status.	
+# VERSION:	1.0
+# CREATED:	17th September, 2025
+# USAGE:	./network-service.sh
+#===============================================================================
+
+
+# Enable 'STRICT MODE' for better error handling
+
+
+
+
+
+
+
+
+
+
+
